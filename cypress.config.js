e0563adeb04255f5ad7277060880012f542a35a8
jack <jack1000sparrow@gmail.com>
2024-05-27 17:03:12 +0500

    signup end to end flow

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    "excludeSpecPattern": ['**/1-getting-started/*','**/2-advanced-examples/*'],
+    "chromeWebSecurity": false,
+    "browser": "chrome",
+    "watchForFileChanges": false,
+    "exprimentalSessionSupport": true,
+    testIsolation: false,
+    "compilerOptions": {
+      "types": ["Cypress"]
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
